c06f1beb7f3c451273d3255eacada1c219ffc66d
Sergio Iglesias <sergioiglesiaslopez03@gmail.com>
2025-12-22 20:02:10 +0100

    None

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import joblib
 from sklearn.base import BaseEstimator
 
-def save_model(model: BaseEstimator, path: str) -> None:
+def save_model(model: BaseEstimator, path: str):
     joblib.dump(model, path)
     print(f"\nThe model has been saved successfully at: {path}\n")
