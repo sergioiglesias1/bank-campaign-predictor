d050bef6dd65e0d4b4e91c361ebef4a13804d810
Sergio Iglesias <sergioiglesiaslopez03@gmail.com>
2026-03-11 11:56:00 +0100

    Clean up imports in visualization.py
    Removed redundant import of RocCurveDisplay.

--- visualization.py
+++ visualization.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, RocCurveDisplay
+from sklearn.metrics import RocCurveDisplay
 
 class Visualizer:
 
